6c15d0e225af9fb339afb35ad9c7e3eeb83973a7
Agung Kurnia Robbi <agung.robbi@gmail.com>
2025-09-19 02:30:55 +0700

    - Set basepath static with repo name - Update images with minified version

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,14 @@
  * @type {import('next').NextConfig}
  */
 
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = '/deskteam360-test';
-
 const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? basePath : '',
-  assetPrefix: isProd ? basePath : '',
+  basePath: '/deskteam360-test',
+  assetPrefix: '/deskteam360-test',
   output: 'export',
   trailingSlash: true,
-  generateBuildId: () => 'build',
 };
 
 module.exports = nextConfig;
